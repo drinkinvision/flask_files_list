82c01fb0f8f6620217bcf45b099b71d1e9ce151e
drinkinvision <139063141+drinkinvision@users.noreply.github.com>
2023-07-11 12:23:28 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -11,17 +11,7 @@ def index():
 def get_files_in_folder():
     folder_path = request.form['folder_path']
     file_list = os.listdir(folder_path)
-
-    save_path = None
-    if 'file_name' in request.form:
-        file_name = request.form['file_name']
-        if file_name.endswith('.txt'):
-            save_path = os.path.join(folder_path, file_name)
-            with open(save_path, 'w') as txt_file:
-                for file in file_list:
-                    txt_file.write(file + '\n')
-
-    return render_template('index.html', files=file_list, save_path=save_path)
+    return render_template('index.html', files=file_list)
 
 if __name__ == '__main__':
     app.run()
